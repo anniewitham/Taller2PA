da4ad37cd5d788dc77b8bc5a5043f1750f2f65e9
Uvitaa <anamaria21qr@gmail.com>
2024-09-16 22:28:48 -0500

    inicio.

---
+++ src/main/java/edu/avanzada/taller2/control/ControlPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.avanzada.taller2.control;
+
+/**
+ *
+ * @author Personal
+ */
+public class ControlPrincipal {
+    
+}
